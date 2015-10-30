37c8ff3c3106d078e135ea46a050a47326ca9135
Freddy Juhel <domaine75@gmail.com>
2015-10-30 13:20:17 +0100

    Initial commit

---
+++ BingPhotoOfDayClient/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+
+namespace BingPhotoOfDayClient
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        BingWallPaperClient client = new BingWallPaperClient();
+        await client.DownloadAsync();
+        BingPhoto.Source = client.WpfPhotoOfTheDay;
+        CopyRightData.Text = client.CoppyRightData;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+    }
+  }
+}
